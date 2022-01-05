8d35b7e96c3f0a7c5f3540d57d57fc8dc95fc06f
Igor Oliveira <57597874+olivigora@users.noreply.github.com>
2022-01-04 23:07:37 -0300

    Update triplex.cpp

--- triplex.cpp
+++ triplex.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 
-int main()
+void PlayGame()
 {
-    // Print welcome messages to the terminal
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    //Print welcome messages to the terminal
+    std::cout << "You are a secret agent breaking into a secure server room...\n";
+    std::cout << "Enter the correct code to continue...\n\n";
 
-    // Declare 3 number code
     const int CodeA = 4;
     const int CodeB = 3;
     const int CodeC = 2;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
-    
+
+
     // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code\n";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    //Store player guesses
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check if the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "ACCESS GRANTED";
+        std::cout << "\nACCESS GRANTED";
     }
-    
     else
     {
-        std::cout << "ACCESS DENIED. SECURITY HAS BEEN NOTIFIED, PLEASE WAIT.";
+        std::cout << "\nACCESS DENIED.\nSECURITY HAS BEEN NOTIFIED.\nPLEASE WAIT.";
     }
+}
 
-
+int main()
+{
+    PlayGame();
     return 0;
 }
+
